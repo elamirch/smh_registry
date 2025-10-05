9c39a9a433d1bead07559157199858f3f2bf594e
elamir <amirrezatm@protonmail.com>
2025-10-05 14:17:23 +0330

    Events and extention emitting added

--- lib.rs
+++ lib.rs
@@ -5,24 +5,50 @@ mod smh_registry {
     use ink::storage::Mapping;
     use ink::prelude::string::String;
 
+    #[ink(event)]
+    pub struct DomainRegistered {
+        #[ink(topic)]
+        domain: String,
+        #[ink(topic)]
+        contract_address: AccountId,
+    }
+
+    #[ink(event)]
+    pub struct DomainExtentionCreated {
+        #[ink(topic)]
+        extention: String,
+    }
+
     #[ink(storage)]
     #[derive(Default)]
     pub struct SmhRegistry {
-        domains: Mapping<String, bool>,
+        domains: Mapping<String, AccountId>,
+        extention: String
     }
 
     impl SmhRegistry {
         #[ink(constructor)]
-        pub fn new() -> Self {
-            Self { domains: Mapping::default() }
+        pub fn new(extention: String) -> Self {
+            let instance = Self {
+                domains: Mapping::default(),
+                extention: extention.clone(),
+            };
+            instance.env().emit_event(DomainExtentionCreated {
+                extention
+            });
+            instance
         }
 
         #[ink(message)]
-        pub fn register_if_free(&mut self, domain: String) -> bool {
+        pub fn register_if_free(&mut self, domain: String, contract_address: AccountId) -> bool {
             if self.domains.get(&domain).is_some() {
                 return false
             }
-            self.domains.insert(&domain, &true);
+            self.domains.insert(&domain, &contract_address);
+            self.env().emit_event(DomainRegistered {
+                domain,
+                contract_address,
+            });
             true
         }
     }
